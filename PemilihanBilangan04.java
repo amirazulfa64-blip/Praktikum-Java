4b3827fc29d426ec5005ec963631ca1e3219b698
Amira Firdausyi Zulfatul Chanifa <email-github-amirazulfa64@gmail.com>
2026-09-14 22:25:38 +0700

    memperbarui modifikasi ternary operator

--- PemilihanBilangan04.java
+++ PemilihanBilangan04.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 public class PemilihanBilangan04 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Masukkan sebuah angka: ");
-        int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + "termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        int angka = input.nextInt();
+        
+        // Modifikasi struktur pemilihan menggunakan ternary operator
+        String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        System.out.println("Angka " + angka + " " + hasil);
 
+        input.close();
     }
 }
